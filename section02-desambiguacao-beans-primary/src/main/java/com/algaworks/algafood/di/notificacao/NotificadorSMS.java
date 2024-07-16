f97b5d6892dceb3e6749fb5d0f567ab8ac3a5c6c
Camila <alvescamila87@gmail.com>
2024-07-16 13:44:02 -0300

    Bean disambiguation with @Primary

--- section02-desambiguacao-beans-primary/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
+++ section02-desambiguacao-beans-primary/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -9,7 +9,7 @@ public class NotificadorSMS implements Notificador {
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s por SMS %s: %s\n",
+		System.out.printf("Notificando %s através do telefone %s: %s\n", 
 				cliente.getNome(), cliente.getTelefone(), mensagem);
 	}
 
--- section02-desambiguacao-beans-primary/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorWhatsapp.java
+++ section02-desambiguacao-beans-primary/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorWhatsapp.java
@@ -1,15 +1,17 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
+//@Primary
 @Component
-public class NotificadorSMS implements Notificador {
+public class NotificadorWhatsapp implements Notificador {
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s por SMS %s: %s\n",
+		System.out.printf("Notificando %s através do Whatsapp %s: %s\n", 
 				cliente.getNome(), cliente.getTelefone(), mensagem);
 	}
 
